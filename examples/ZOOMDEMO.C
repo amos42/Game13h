3770c6146c8146250ffe0221f261ff15e0cf2678
Ju, Gyeong-min <jcmh74@gmail.com>
2025-06-07 19:06:27 +0900

    빌드 바이너리 추가
    Signed-off-by: Ju, Gyeong-min <jcmh74@gmail.com>

--- examples/ZOOMDEMO.C
+++ examples/ZOOMDEMO.C
@@ -1,12 +1,26 @@
 #include <game13h.h>
 
+void put_zoom_image( image_data image, int x, int y )
+{
+  image_data image_t;
+
+  image_zoom( image, &image_t, x, y );
+
+  bar( 10, 10, 10+x, 10+y, BLACK );
+  copy_put( 10, 10, image_t );
+
+  farfree( image_t );
+}
+
 main()
 {
-  int ch, x = 1, y = 1;
-  image_data image, image_t;
+  int ch, x = 100, y = 100;
+  image_data image;
 
   init_game13h();
   load_image( "hose.img", &image, 0 );
+
+  put_zoom_image( image, x, y );
 
   do{
     ch = get_key();
@@ -20,10 +34,7 @@ main()
     if( x < 1 ) x = 1;
     if( y < 1 ) y = 1;
 
-    image_zoom( image, &image_t, x, y );
-    bar( 10, 10, 10+x, 10+y, BLACK );
-    copy_put( 10, 10, image_t );
-    farfree( image_t );
+    put_zoom_image( image, x, y );
   } while( ch != ESC );
 
   farfree( image );
